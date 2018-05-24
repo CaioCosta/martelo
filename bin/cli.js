a093031045bcbd2b1918c35b5097403ac48fc10b
Caio Costa <caiocrcosta@gmail.com>
2018-05-24 18:43:36 -0300

    Make the CLI grab the config from a list of possible files
    The CLI will get the build config from the following list:
    - `--config` parameter if set, or
    - `martelo.config.js`, or
    - `martelo.config.json`, or
    - `martelo` key in `package.json`.
    
    If no config is found after all those tries, the CLI will exit with a
    non-zero error code.

--- bin/cli.js
+++ bin/cli.js
@@ -11,32 +11,73 @@ const Martelo = require("../Martelo");
 (async () => {
 	"use strict";
 
-	let config = {};
+	const filesToCheck = [
+		"martelo.config.js",
+		"martelo.config.json",
+		"package.json",
+	];
+
+	let config = null;
 	let configFilePath = null;
 
+	// Config file parameter is set, let's add it to the top of the file check list
 	if (args.config !== null) {
-		const configFileStat = await FsPromise.stat(args.config)
+		filesToCheck.unshift(args.config);
+	}
+
+	for (const filePath of filesToCheck) {
+		const fileStat = await FsPromise.stat(filePath)
 			.catch(() => {
-				log(`File ${log.formatFilename(args.config)} doesn't exist`, log.level.FATAL);
+				const message = `File ${log.formatFilename(filePath)} doesn't exist`;
+
+				// Config file was forced, but not found. We'll exit now.
+				if (filePath === args.config) {
+					log(message, log.level.FATAL);
+
+					process.exit(1);
+				}
+				else {
+					log(message, log.level.INFO);
+				}
 			});
 
-		if (configFileStat === void 0) {
-			return;
+		// Config file is found; break out of the loop
+		if (fileStat !== void 0) {
+			configFilePath = filePath;
+
+			log(`Using config from ${log.formatFilename(filePath)}`, log.level.INFO);
+
+			break;
+		}
+	}
+
+	if (configFilePath !== null) {
+		config = require(path.resolve(configFilePath));
+
+		if (configFilePath === "package.json") {
+			config = config.martelo;
+
+			if (config === void 0) {
+				log(`There's no config in ${log.formatFilename("package.json")}`, log.level.ERROR);
+			}
 		}
 
-		configFilePath = path.resolve(args.config);
+		if (config !== void 0) {
+			const martelo = new Martelo(config, {
+				environment: args._[0],
+				partial: args._[1],
+			});
+
+			return martelo.build();
+		}
 	}
 
-	if (configFilePath === null) {
-		configFilePath = "martelo.config";
+	if (!config) {
+		log(
+			"No configuration file has been found.\n"
+			+ "Please refer to the guide at https://github.com/CaioCosta/martelo to know how to "
+			+ "configure your project.",
+			log.level.FATAL
+		);
 	}
-
-	config = require(configFilePath);
-
-	const martelo = new Martelo(config, {
-		environment: args._[0],
-		partial: args._[1],
-	});
-
-	return martelo.build();
 })();
