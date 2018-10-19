a68e0a19c37e25b03e6e0db14c84ddff3d2175a6
Caio Costa <caiocrcosta@gmail.com>
2018-10-18 22:39:52 -0300

    Add `sync` cli option

--- bin/cli.js
+++ bin/cli.js
@@ -46,4 +46,27 @@ commander
 		return martelo.watch();
 	});
 
+commander
+	.command("sync [environment]")
+	.alias("s")
+	.description(
+		"Creates a Browsersync instance to refresh the browser with the changes in built files"
+	)
+	.option("-b, --build", "Builds the project before spanning the Browsersync instance")
+	.option("-w, --watch", "Watches for changes in source files along with Browsersync instance")
+	.action(async (environment, args) => {
+		const martelo = await createMarteloInstance(environment);
+		const browserSync = require("browser-sync").create();
+
+		if (args.build) {
+			await martelo.build();
+		}
+
+		if (args.watch) {
+			martelo.watch(browserSync);
+		}
+
+		return martelo.sync(browserSync);
+	});
+
 commander.parse(process.argv);
